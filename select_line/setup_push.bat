f2c3e72be5d5452cce7599879c1985e84a306b1f
fifoteam <haitaox2008@gmail.com>
2017-05-09 17:51:10 +0800

    AUTO SAVED BY BAT FILE

--- select_line/setup_push.bat
+++ select_line/setup_push.bat
@@ -2,7 +2,7 @@ echo off
 echo %1
 ::½øÈëÄ¿Â¼
 cd /d %1
-@ start	"" "python" setup.py py2exe
+@ start /wait	"" "python" setup.py py2exe
 rd __pycache__ /s/q
 echo "python temp file del done!"
 git add *
