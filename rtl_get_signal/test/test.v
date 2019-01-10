d3fe268ac32a82edf12698ce93c9246f8abf59e1
fifoteam <haitaox2008@163.com>
2019-01-10 20:20:39 +0800

    AUTO SAVED BY BAT FILE

---
+++ rtl_get_signal/test/test.v
@@ -0,0 +1,5 @@
+	assign	current_idle = current_state == S_IDLE	;
+	assign	current_gvcp = current_state == S_GVCP	;
+	assign	current_mac	 = current_state == S_MAC	;
+	assign	current_gvsp = current_state == S_GVSP	;
+	assign	current_gap	 = current_state == S_GAP	;
